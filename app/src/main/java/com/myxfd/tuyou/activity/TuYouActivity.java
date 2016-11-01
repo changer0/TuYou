f15305be6a06791fc9e68d70e3dc461f8d3f8335
luxinyi <luxinyi93@sina.com>
2016-11-01 16:14:28 +0800

    修改联动bug

--- app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
+++ app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
@@ -20,7 +20,7 @@ import com.myxfd.tuyou.fragments.MineFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TuYouActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+public class TuYouActivity extends AppCompatActivity {
     private Fragment circleFragment, mineFragment, mapFragment, messageFragment;
     private FragmentManager manager;
     private ViewPager pager;
@@ -33,22 +33,7 @@ public class TuYouActivity extends AppCompatActivity implements TabLayout.OnTabS
         setContentView(R.layout.activity_tu_you);
         pager = (ViewPager) findViewById(R.id.main_container);
         tabLayout = (TabLayout) findViewById(R.id.main_ty);
-        //创建tab
-        TabLayout.Tab tab = tabLayout.newTab();
-        tab.setText("消息");
-        tabLayout.addTab(tab);
 
-        tab = tabLayout.newTab();
-        tab.setText("地图");
-        tabLayout.addTab(tab);
-
-        tab = tabLayout.newTab();
-        tab.setText("图友圈");
-        tabLayout.addTab(tab);
-
-        tab = tabLayout.newTab();
-        tab.setText("我的");
-        tabLayout.addTab(tab);
         //添加ViewPager
         manager = getSupportFragmentManager();
         List<BaseFragment> fragments = new ArrayList<>();
@@ -57,23 +42,11 @@ public class TuYouActivity extends AppCompatActivity implements TabLayout.OnTabS
         fragments.add(new CircleFragment());
         fragments.add(new MineFragment());
         //设置tabLayout
-        tabLayout.addOnTabSelectedListener(this);
+
         adapter = new CommonFragmentPagerAdapter(manager, fragments);
         pager.setAdapter(adapter);
+        //让tabLayout与ViewPager联动
+        tabLayout.setupWithViewPager(pager);
     }
 
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        pager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
-    }
 }
