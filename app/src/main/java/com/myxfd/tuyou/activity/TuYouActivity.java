71472e40bf9e60638590259ccc18fc2bf7b039e6
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-11-02 10:16:24 +0800

    完成高德地图的基本定位服务

--- app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
+++ app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
@@ -1,6 +1,7 @@
 package com.myxfd.tuyou.activity;
 
 
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,8 +9,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import com.amap.api.maps2d.SupportMapFragment;
 import com.myxfd.tuyou.R;
 import com.myxfd.tuyou.adapters.CommonFragmentPagerAdapter;
 import com.myxfd.tuyou.fragments.BaseFragment;
@@ -17,23 +20,25 @@ import com.myxfd.tuyou.fragments.CircleFragment;
 import com.myxfd.tuyou.fragments.MapFragment;
 import com.myxfd.tuyou.fragments.MessageFragment;
 import com.myxfd.tuyou.fragments.MineFragment;
+import com.myxfd.tuyou.widgets.ViewPagerCompat;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TuYouActivity extends AppCompatActivity {
-    private Fragment circleFragment, mineFragment, mapFragment, messageFragment;
+public class TuYouActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, RadioGroup.OnCheckedChangeListener {
+
     private FragmentManager manager;
-    private ViewPager pager;
+    private ViewPagerCompat pager;
     private CommonFragmentPagerAdapter adapter;
-    private TabLayout tabLayout;
+    private RadioGroup mRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tu_you);
-        pager = (ViewPager) findViewById(R.id.main_container);
-        tabLayout = (TabLayout) findViewById(R.id.main_ty);
+        pager = (ViewPagerCompat) findViewById(R.id.main_container);
+
 
         //添加ViewPager
         manager = getSupportFragmentManager();
@@ -42,12 +47,62 @@ public class TuYouActivity extends AppCompatActivity {
         fragments.add(new MapFragment());
         fragments.add(new CircleFragment());
         fragments.add(new MineFragment());
-        //设置tabLayout
 
         adapter = new CommonFragmentPagerAdapter(manager, fragments);
         pager.setAdapter(adapter);
-        //让tabLayout与ViewPager联动
-        tabLayout.setupWithViewPager(pager);
+        pager.addOnPageChangeListener(this);
+
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.main_rg);
+
+        mRadioGroup.setOnCheckedChangeListener(this);
+        ((RadioButton) mRadioGroup.getChildAt(0)).setChecked(true);
+
     }
 
+
+    // -------------------------------------
+    // Viewpager的监听回调
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        View childAt = mRadioGroup.getChildAt(position);
+        if (childAt instanceof RadioButton) {
+            RadioButton radioButton = (RadioButton) childAt;
+            radioButton.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    // ------------------------------------------
+    // RadioGroup的回调监听
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.main_rb_msg:
+                pager.setCurrentItem(0);
+                break;
+            case R.id.main_rb_map:
+                pager.setCurrentItem(1);
+                break;
+
+            case R.id.main_rb_circle:
+
+                pager.setCurrentItem(2);
+                break;
+            case R.id.main_rb_mine:
+                pager.setCurrentItem(3);
+                break;
+
+
+        }
+    }
 }
--- app/src/main/java/com/myxfd/tuyou/fragments/MapFragment.java
+++ app/src/main/java/com/myxfd/tuyou/fragments/MapFragment.java
@@ -1,22 +1,81 @@
 package com.myxfd.tuyou.fragments;
 
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
 import com.myxfd.tuyou.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends BaseFragment {
+public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickListener {
 
+    private static final String TAG = "MapFragment";
+
+    private MapView mapView;
+
+    private Fragment circleFragment, mineFragment, mapFragment, messageFragment;
+    private FragmentManager manager;
+    private AMap aMap;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //配置信息
+    private AMapLocationClientOption mLocationOption;
+
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation location) {
+
+            if (location.getErrorCode() == 0) {
+                //定位成功
+                Log.d(TAG, "onLocationChanged: location => " + location.getLongitude() + " : " + location.getLatitude());
+
+                //将定位信息显示到地图上
+                MarkerOptions markerOptions = new MarkerOptions();
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                markerOptions.position(latLng)
+                        .title(location.getCity())
+                        .snippet(location.getAddress())
+                        .draggable(false)//是否拖拽
+                        .icon(BitmapDescriptorFactory.fromBitmap(
+                                BitmapFactory.decodeResource(getResources(), R.mipmap.poi_marker_pressed)
+                        ));
+
+                //添加到地图上
+                aMap.addMarker(markerOptions);
+
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + location.getErrorCode() + ", errInfo:"
+                        + location.getErrorInfo());
+            }
+
+
+        }
+    };
 
     public MapFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
@@ -28,8 +87,80 @@ public class MapFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View itemView = inflater.inflate(R.layout.fragment_map, container, false);
+        //获取地图引用
+        mapView = (MapView) itemView.findViewById(R.id.map_view);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
+        mapView.onCreate(savedInstanceState);
+        //给aMap赋值
+        aMap = mapView.getMap();
+        //信息窗体, 点击回调
+        aMap.setOnInfoWindowClickListener(this);
+
+        //开启定位
+        startLocation();
+
+        return itemView;
+    }
+
+    //定位方法
+    private void startLocation() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+
+
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+//        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(5000);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+    // --------------------------------------------------
+    // 信息窗体回调监听
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        marker.hideInfoWindow();//隐藏信息窗体
+        Toast.makeText(getContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+    }
+    // -------------------------------------------------------
+    // 生命周期方法
+    @Override
+    public void onDestroy() {
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
     }
 
 }
