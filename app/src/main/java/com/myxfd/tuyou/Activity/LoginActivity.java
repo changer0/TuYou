92e347e41f16d6a01b8065ddf3ab1511143e9ad4
fushuangdage <463430762@qq.com>
2016-11-02 10:14:29 +0800

    Merge remote-tracking branch 'origin/master' 根据QQ注册用户

--- app/src/main/java/com/myxfd/tuyou/Activity/LoginActivity.java
+++ app/src/main/java/com/myxfd/tuyou/Activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.myxfd.tuyou.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +12,22 @@ import android.view.View;
 import android.widget.Button;
 
 import com.myxfd.tuyou.R;
-import com.myxfd.tuyou.adapter.CommonFragmentPagerAdapter;
-import com.myxfd.tuyou.fragment.AcountLoginFragment;
-import com.myxfd.tuyou.fragment.LoginBaseFragment;
-import com.myxfd.tuyou.fragment.PhoneLoginFragment;
+import com.myxfd.tuyou.adapters.CommonFragmentPagerAdapter;
+import com.myxfd.tuyou.fragments.AcountLoginFragment;
+import com.myxfd.tuyou.fragments.BaseFragment;
+import com.myxfd.tuyou.fragments.PhoneLoginFragment;
+import com.myxfd.tuyou.model.TuYouUser;
+import com.myxfd.tuyou.tools.Tools;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
-import com.myxfd.tuyou.model.TuYouUser;
-
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
@@ -34,13 +40,15 @@ import cn.sharesdk.wechat.friends.Wechat;
 public class LoginActivity extends AppCompatActivity implements PlatformActionListener, View.OnClickListener {
     
     private static final String TAG = "LoginActivity";
-    private ArrayList<LoginBaseFragment> mArrayList;
+    private ArrayList<BaseFragment> mArrayList;
     private String currentPlatName;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mContext=this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(toolbar);
 
@@ -66,16 +74,55 @@ public class LoginActivity extends AppCompatActivity implements PlatformActionLi
 
     }
 
-
-
     @Override
     public void onComplete(Platform platform, int i, HashMap<String, Object> map) {
         String name = platform.getName();
         currentPlatName = name;
         Log.d(TAG, "onComplete: name =>" + name);
+        switch (name){
+            case "QQ":
+//                Iterator<Map.Entry<String, Object>> ite = map.entrySet().iterator();
+                final String nickname = (String) map.get("nickname");
+                final String tuYouName = Tools.getRandomTuYouName();
+                final String tuYouPwd="12345678";
+                BmobQuery<TuYouUser> query = new BmobQuery<>();
+                query.addWhereEqualTo("username",currentPlatName);
+                query.findObjects(new FindListener<TuYouUser>() {
+                    @Override
+                    public void done(List<TuYouUser> list, BmobException e) {
+                        if (list.size() ==0) {
+                            //用户不存在
+                            TuYouUser user = new TuYouUser();
+                            user.setUsername(nickname);
+                            user.setPassword(tuYouPwd);
+                            user.setType(currentPlatName);
+                            user.signUp(new SaveListener<TuYouUser>() {
+                                @Override
+                                public void done(TuYouUser tuYouUser, BmobException e) {
+                                    if (e != null) {
+                                        Log.d(TAG, "done1: "+e);
+                                        int errorCode = e.getErrorCode();
+                                        if (errorCode==202){
+                                            Log.d(TAG, "done: 当前QQ已经注册过了");
+                                        }
+                                    }else {
+                                        Intent intent = new Intent(mContext, MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        }else {
+                            Log.d(TAG, "done2: "+e);
+                        }
+                    }
+                });
+
+                break;
+        }
         //遍历Map
         Iterator ite =map.entrySet().iterator();
         while (ite.hasNext()) {
+            Log.d(TAG, "onComplete:  ");
             Map.Entry entry = (Map.Entry)ite.next();
             Object key = entry.getKey();
             Object value = entry.getValue();
@@ -92,8 +139,6 @@ public class LoginActivity extends AppCompatActivity implements PlatformActionLi
     public void onCancel(Platform platform, int i) {
 
     }
-
-
     @Override
     public void onClick(View v) {
         int id = v.getId();
