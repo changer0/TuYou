07f732f469898909a758be7c4fd7f93b4f6fd8ab
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-11-02 11:54:52 +0800

    完成未测试的附近功能, 需要与付爽的登陆整合再测试

--- app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
+++ app/src/main/java/com/myxfd/tuyou/activity/TuYouActivity.java
@@ -1,6 +1,7 @@
 package com.myxfd.tuyou.activity;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -20,6 +21,7 @@ import com.myxfd.tuyou.fragments.CircleFragment;
 import com.myxfd.tuyou.fragments.MapFragment;
 import com.myxfd.tuyou.fragments.MessageFragment;
 import com.myxfd.tuyou.fragments.MineFragment;
+import com.myxfd.tuyou.services.UploadNearbyInfoService;
 import com.myxfd.tuyou.widgets.ViewPagerCompat;
 
 
@@ -39,6 +41,8 @@ public class TuYouActivity extends AppCompatActivity implements ViewPager.OnPage
         setContentView(R.layout.activity_tu_you);
         pager = (ViewPagerCompat) findViewById(R.id.main_container);
 
+        // TODO: 2016/11/2 开启位置上传服务!!!! 以后必须修改到闪屏页中
+        startService(new Intent(this, UploadNearbyInfoService.class));
 
         //添加ViewPager
         manager = getSupportFragmentManager();
--- app/src/main/java/com/myxfd/tuyou/fragments/MapFragment.java
+++ app/src/main/java/com/myxfd/tuyou/fragments/MapFragment.java
@@ -2,6 +2,7 @@ package com.myxfd.tuyou.fragments;
 
 
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,17 +17,27 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdate;
+import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.nearby.NearbyInfo;
+import com.amap.api.services.nearby.NearbySearch;
+import com.amap.api.services.nearby.NearbySearchFunctionType;
+import com.amap.api.services.nearby.NearbySearchResult;
 import com.myxfd.tuyou.R;
+import com.myxfd.tuyou.utils.MapUtil;
+
+import static cn.bmob.v3.Bmob.getApplicationContext;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickListener {
+public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickListener, NearbySearch.NearbyListener {
 
     private static final String TAG = "MapFragment";
 
@@ -51,7 +62,9 @@ public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickL
 
                 //将定位信息显示到地图上
                 MarkerOptions markerOptions = new MarkerOptions();
+                //得到定位的位置
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                //设置标记物
                 markerOptions.position(latLng)
                         .title(location.getCity())
                         .snippet(location.getAddress())
@@ -62,10 +75,18 @@ public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickL
 
                 //添加到地图上
                 aMap.addMarker(markerOptions);
+                //放大到最高级别
+                CameraUpdate zoom = CameraUpdateFactory.zoomTo(20);
+                aMap.animateCamera(zoom);
 
+                //始终保持当前位置是屏幕中心
+                CameraUpdate changeLatLng = CameraUpdateFactory.changeLatLng(latLng);
+                aMap.animateCamera(changeLatLng);
+
+                Toast.makeText(getContext(), "定位成功", Toast.LENGTH_SHORT).show();
             } else {
                 //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
-                Log.e("AmapError", "location Error, ErrCode:"
+                Log.e(TAG, "location Error, ErrCode:"
                         + location.getErrorCode() + ", errInfo:"
                         + location.getErrorInfo());
             }
@@ -114,13 +135,13 @@ public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickL
 
         //初始化AMapLocationClientOption对象
         mLocationOption = new AMapLocationClientOption();
-        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
-//        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+//        设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+//        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
 
         //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
-        mLocationOption.setInterval(5000);
+        mLocationOption.setInterval(1000);
         //设置是否返回地址信息（默认返回地址信息）
         mLocationOption.setNeedAddress(true);
         //给定位客户端对象设置定位参数
@@ -136,31 +157,94 @@ public class MapFragment extends BaseFragment implements AMap.OnInfoWindowClickL
         marker.hideInfoWindow();//隐藏信息窗体
         Toast.makeText(getContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
     }
+
+
+    // ---------------------------------------------------
+    //在地图中添加其他用户的位置
+    private void addOtherUser() {
+        //设置搜索条件
+        NearbySearch.NearbyQuery query = new NearbySearch.NearbyQuery();
+
+        //设置搜索的中心点
+        query.setCenterPoint(MapUtil.getCurrentUserLatLonPoint());
+        //设置搜索的坐标体系
+        query.setCoordType(NearbySearch.AMAP);
+        //设置搜索半径
+        query.setRadius(1000);
+        //设置查询的时间
+        query.setTimeRange(10000);
+        //设置查询的方式驾车还是距离
+        query.setType(NearbySearchFunctionType.DRIVING_DISTANCE_SEARCH);
+        //调用异步查询接口
+        NearbySearch.getInstance(getApplicationContext())
+                .searchNearbyInfoAsyn(query);
+        //添加回调监听
+        NearbySearch.getInstance(getApplicationContext()).addNearbyListener(this);
+    }
+
+    @Override
+    public void onUserInfoCleared(int i) {
+    }
+
+    @Override
+    public void onNearbyInfoSearched(NearbySearchResult nearbySearchResult, int resultCode) {
+        //搜索周边附近用户回调处理
+        if(resultCode == 1000){
+            if (nearbySearchResult != null
+                    && nearbySearchResult.getNearbyInfoList() != null
+                    && nearbySearchResult.getNearbyInfoList().size() > 0) {
+                NearbyInfo nearbyInfo = nearbySearchResult.getNearbyInfoList().get(0);
+
+                // TODO: 2016/11/2 待完成!!!
+                
+            } else {
+                Log.d(TAG, "onNearbyInfoSearched: 周边搜索结果为空");
+            }
+        }
+        else{
+            Log.d(TAG, "onNearbyInfoSearched: 周边搜索出现异常，异常码为：" + resultCode);
+        }
+    }
+
+    @Override
+    public void onNearbyInfoUploaded(int i) {
+
+    }
+
+
     // -------------------------------------------------------
     // 生命周期方法
     @Override
     public void onDestroy() {
+        //停止定位
+        mLocationClient.stopLocation();//停止定位后，本地定位服务并不会被销毁
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mapView.onDestroy();
+        //释放附近资源
+        NearbySearch.destroy();
         super.onDestroy();
     }
+
     @Override
     public void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
+
     @Override
     public void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
         mapView.onSaveInstanceState(outState);
     }
+
 
 }
