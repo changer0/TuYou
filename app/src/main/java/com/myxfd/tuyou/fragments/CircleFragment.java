147be6faea184680720e95643b09688771e56e80
fushuangdage <463430762@qq.com>
2016-11-02 11:37:21 +0800

    Merge remote-tracking branch 'origin/master' 可以发说说啦,不完善

--- app/src/main/java/com/myxfd/tuyou/fragments/CircleFragment.java
+++ app/src/main/java/com/myxfd/tuyou/fragments/CircleFragment.java
@@ -1,19 +1,25 @@
 package com.myxfd.tuyou.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.widget.Button;
 
 import com.myxfd.tuyou.R;
+import com.myxfd.tuyou.activity.EditCircleMsgActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CircleFragment extends BaseFragment {
+public class CircleFragment extends BaseFragment implements View.OnClickListener {
 
+
+    private View mWebView;
 
     public CircleFragment() {
         // Required empty public constructor
@@ -29,7 +35,18 @@ public class CircleFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_circle, container, false);
+        View view = inflater.inflate(R.layout.fragment_circle, container, false);
+        Button button = (Button) view.findViewById(R.id.circle_sendShuoShuo);
+        button.setOnClickListener(this);
+        mWebView = view.findViewById(R.id.circle_webView);
+        //开启js支持
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getContext(), EditCircleMsgActivity.class);
+        startActivity(intent);
+    }
 }
