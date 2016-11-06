ab1fbea9f7f265f291e2f19b40a8d346a680c29b
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-11-06 10:58:47 +0800

    解决已知bug, 增加下方图标

--- app/src/main/java/com/myxfd/tuyou/Activity/LoginActivity.java
+++ app/src/main/java/com/myxfd/tuyou/Activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.myxfd.tuyou.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.myxfd.tuyou.R;
 import com.myxfd.tuyou.adapters.CommonFragmentPagerAdapter;
@@ -17,7 +18,7 @@ import com.myxfd.tuyou.fragments.AcountLoginFragment;
 import com.myxfd.tuyou.fragments.BaseFragment;
 import com.myxfd.tuyou.fragments.PhoneLoginFragment;
 import com.myxfd.tuyou.model.TuYouUser;
-import com.myxfd.tuyou.tools.Tools;
+import com.myxfd.tuyou.utils.Tools;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,6 +32,7 @@ import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.PlatformDb;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.sina.weibo.SinaWeibo;
 import cn.sharesdk.tencent.qq.QQ;
@@ -57,66 +59,70 @@ public class LoginActivity extends AppCompatActivity implements PlatformActionLi
         tabLayout.setupWithViewPager(viewPager);
 
         mArrayList = new ArrayList<>();
+        //添加账户登录和手机号快捷登陆的两个Fragment
         mArrayList.add(new AcountLoginFragment());
         mArrayList.add(new PhoneLoginFragment());
         CommonFragmentPagerAdapter adapter = new CommonFragmentPagerAdapter(getSupportFragmentManager(), mArrayList);
         viewPager.setAdapter(adapter);
 
-        Button qqBtn = (Button) findViewById(R.id.other_login_btn_qq);
-        Button sinaBtn = (Button) findViewById(R.id.other_login_btn_sina);
-        Button weiChatBtn = (Button) findViewById(R.id.other_login_btn_wei_chat);
-        Button cancelBtn = (Button) findViewById(R.id.other_login_btn_cancel);
+        TextView qqBtn = (TextView) findViewById(R.id.other_login_btn_qq);
+        TextView sinaBtn = (TextView) findViewById(R.id.other_login_btn_sina);
 
         qqBtn.setOnClickListener(this);
         sinaBtn.setOnClickListener(this);
-        weiChatBtn.setOnClickListener(this);
-        cancelBtn.setOnClickListener(this);
 
     }
 
     @Override
     public void onComplete(Platform platform, int i, HashMap<String, Object> map) {
         String name = platform.getName();
+
         currentPlatName = name;
         Log.d(TAG, "onComplete: name =>" + name);
         switch (name) {
-            case "QQ":
-//                Iterator<Map.Entry<String, Object>> ite = map.entrySet().iterator();
-                final String nickname = (String) map.get("nickname");
-                final String tuYouName = Tools.getRandomTuYouName();
-                final String tuYouPwd = "12345678";
-                BmobQuery<TuYouUser> query = new BmobQuery<>();
-                query.addWhereEqualTo("username", currentPlatName);
-                query.findObjects(new FindListener<TuYouUser>() {
+            case "QQ" :
+                TuYouUser user = new TuYouUser();
+                String tuYouPwd = "mustChange";
+                String username = "TuYou" + platform.getDb().getToken();
+
+
+                String sex = (String) map.get("gender");
+                String icon = (String) map.get("figureurl_qq_1");
+                user.setUsername(username);
+                user.setPassword(tuYouPwd);
+                user.setSex(sex);
+                user.setIcon(icon);
+
+                user.setType(currentPlatName);
+
+                user.signUp(new SaveListener<TuYouUser>() {
                     @Override
-                    public void done(List<TuYouUser> list, BmobException e) {
-                        if (list.size() == 0) {
-                            //用户不存在
-                            TuYouUser user = new TuYouUser();
-                            user.setUsername(nickname);
-                            user.setPassword(tuYouPwd);
-                            user.setType(currentPlatName);
-                            user.signUp(new SaveListener<TuYouUser>() {
-                                @Override
-                                public void done(TuYouUser tuYouUser, BmobException e) {
-                                    if (e != null) {
-                                        Log.d(TAG, "done1: " + e);
-                                        int errorCode = e.getErrorCode();
-                                        if (errorCode == 202) {
-                                            Log.d(TAG, "done: 当前QQ已经注册过了");
-                                        }
-                                    } else {
-                                        Intent intent = new Intent(mContext, TuYouActivity.class);
-                                        startActivity(intent);
-                                    }
-                                }
-                            });
+                    public void done(TuYouUser tuYouUser, BmobException e) {
+                        if (e != null) {
+                            Log.d(TAG, "done1: " + e);
+                            int errorCode = e.getErrorCode();
+                            if (errorCode == 202) {
+                                Log.d(TAG, "done: 当前QQ已经注册过了");
+                                Intent intent = new Intent(mContext, TuYouActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // TODO: 2016/11/6 处理未知异常
+
+                            }
                         } else {
-                            Log.d(TAG, "done2: " + e);
+                            Intent intent = new Intent(mContext, TuYouActivity.class);
+                            startActivity(intent);
+                            finish();
                         }
                     }
                 });
 
+
+                break;
+
+            case "SinaWeibo":
+                Snackbar.make(getWindow().getDecorView(), "程序猿正在努力完成中......", Snackbar.LENGTH_SHORT).show();
                 break;
         }
         //遍历Map
@@ -152,10 +158,7 @@ public class LoginActivity extends AppCompatActivity implements PlatformActionLi
             case R.id.other_login_btn_sina:
                 platform = ShareSDK.getPlatform(this, SinaWeibo.NAME);
                 break;
-            case R.id.other_login_btn_wei_chat:
-                platform = ShareSDK.getPlatform(this, Wechat.NAME);
-
-                break;
+            // TODO: 2016/11/5 取消授权, 在登出中使用
             case R.id.other_login_btn_cancel:
                 //取消授权
                 if (currentPlatName != null) {
