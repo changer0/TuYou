317a101adf52d0a93bb88b07182572bfe64fb995
liangyue <nsliangyue@163.com>
2016-11-07 17:21:37 +0800

    解决环信登录不同账号的问题

--- app/src/main/java/com/myxfd/tuyou/Activity/RegisterActivity.java
+++ app/src/main/java/com/myxfd/tuyou/Activity/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.myxfd.tuyou.activity;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.exceptions.HyphenateException;
 import com.myxfd.tuyou.R;
@@ -25,6 +28,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private Button register;
     private static final String TAG = "RegisterActivity";
     private EditText password2;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(RegisterActivity.this, TuYouActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,54 +59,73 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 final String userPassword = password.getText().toString().trim();
                 String userPassword2 = password2.getText().toString().trim();
                 if (userPassword.equals(userPassword2)) {
-                    TuYouUser user = new TuYouUser();
-                    user.setUsername(userName);
-                    user.setPassword(userPassword);
-                    final TuYouUser tempUser = user;
-                    user.signUp(new SaveListener<TuYouUser>() { //注册
+                    new Thread() {
                         @Override
-                        public void done(TuYouUser tuYouUser, BmobException e) {
-                            if (e == null) {
-                                //立即登录, 登录成功后跳转
-                                tempUser.login(new SaveListener<TuYouUser>() { //登录
+                        public void run() {
+                            try {
+                                EMClient.getInstance().createAccount(userName, userPassword);
+                                EMClient.getInstance().logout(true);
+                                EMClient.getInstance().login(userName, userPassword, new EMCallBack() {
+                                    @Override
+                                    public void onSuccess() {
+                                        EMClient.getInstance().chatManager().loadAllConversations();
+                                        EMClient.getInstance().groupManager().loadAllGroups();
+                                    }
+
+                                    @Override
+                                    public void onError(int i, String s) {
+
+                                    }
+
+                                    @Override
+                                    public void onProgress(int i, String s) {
+
+                                    }
+                                });
+                                TuYouUser user = new TuYouUser();
+                                user.setUsername(userName);
+                                user.setPassword(userPassword);
+                                final TuYouUser tempUser = user;
+                                user.signUp(new SaveListener<TuYouUser>() { //注册
                                     @Override
                                     public void done(TuYouUser tuYouUser, BmobException e) {
                                         if (e == null) {
-                                            Intent intent = new Intent(RegisterActivity.this, TuYouActivity.class);
-                                            startActivity(intent);
+                                            //立即登录, 登录成功后跳转
+                                            tempUser.login(new SaveListener<TuYouUser>() { //登录
+                                                @Override
+                                                public void done(TuYouUser tuYouUser, BmobException e) {
+                                                    if (e == null) {
+                                                        handler.obtainMessage(10086).sendToTarget();
+                                                    } else {
+                                                        Snackbar.make(getWindow().getDecorView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
                                         } else {
-                                            Snackbar.make(getWindow().getDecorView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                            // 网络异常: 9016
+                                            if (e.getErrorCode() == 9016) {
+                                                Snackbar.make(getWindow().getDecorView(), "亲, 请连接网络 ヾ(≧O≦)〃嗷~", Snackbar.LENGTH_SHORT).show();
+                                            } else if (e.getErrorCode() == 304) {
+                                                Snackbar.make(getWindow().getDecorView(), "亲, 用户名密码不能为空....", Snackbar.LENGTH_SHORT).show();
+                                            } else if (e.getErrorCode() == 202) {
+                                                Snackbar.make(getWindow().getDecorView(), "该用户名已经注册, 请重试..", Snackbar.LENGTH_SHORT).show();
+                                            } else {
+                                                Snackbar.make(getWindow().getDecorView(), "未知错误", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                            Log.d(TAG, "done: message: " + e.getMessage() + " code:" + e.getErrorCode());
                                         }
                                     }
                                 });
-                            } else {
-                                // 网络异常: 9016
-                                if (e.getErrorCode() == 9016) {
-                                    Snackbar.make(getWindow().getDecorView(), "亲, 请连接网络 ヾ(≧O≦)〃嗷~", Snackbar.LENGTH_SHORT).show();
-                                } else if (e.getErrorCode() == 304) {
-                                    Snackbar.make(getWindow().getDecorView(), "亲, 用户名密码不能为空....", Snackbar.LENGTH_SHORT).show();
-                                } else if (e.getErrorCode() == 202) {
-                                    Snackbar.make(getWindow().getDecorView(), "该用户名已经注册, 请重试..", Snackbar.LENGTH_SHORT).show();
-                                } else {
-                                    Snackbar.make(getWindow().getDecorView(), "未知错误", Snackbar.LENGTH_SHORT).show();
-                                }
-                                Log.d(TAG, "done: message: " + e.getMessage() + " code:" + e.getErrorCode());
+                            } catch (HyphenateException e) {
+                                e.printStackTrace();
                             }
                         }
-                    });
+                    }.start();
+
                 } else {
                     Snackbar.make(getWindow().getDecorView(), "密码不一致,请重新输入", Snackbar.LENGTH_SHORT).show();
                 }
-                new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            EMClient.getInstance().createAccount(userName, userPassword);
-                        } catch (HyphenateException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }.start();
+
                 break;
         }
     }
